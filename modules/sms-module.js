53999f9f85a46511ccbd22d455f28134dbef0597
Avichal Rakesh <avichal.rakesh@gmail.com>
2017-11-26 03:44:07 -0600

    add module to add messages to db

---
+++ modules/sms-module.js
@@ -0,0 +1,50 @@
+module.exports = {};
+
+module.exports.addSms = function (messages, res, db) {
+    console.log(messages);
+
+    var hadError = false;
+
+    var latestTime = -1;
+    var ctr = 0;
+    var total = messages.total;
+    var messageList = messages.items;
+    for (var i = 0; i < total; i = i + 1) {
+        if (messageList[i].datetime > latestTime) {
+            latestTime = messageList[i].datetime;
+        }
+
+        db.collection('messages').save(messageList[i], function(err, result) {
+            ctr = ctr + 1;
+            if (err) {
+                hadError = true;
+                if (ctr == total) {
+                    sendResponse(hadError, latestTime, res);
+                }
+                return console.log(err);
+            }
+            console.log(i);
+            if (ctr == total) {
+                sendResponse(hadError, latestTime, res);
+            }
+        });
+    }
+}
+
+function sendResponse(hadError, latestTime, res) {
+    if (hadError) {
+        var errObj = {
+            "status" : 1,
+            "err_message" : "Failed to add some messages to database"
+        };
+        res.send(errObj);
+    } else {
+        var succObj = {
+            "status" : 0,
+            "success_data" : {
+                "latest_time" : latestTime
+            }
+        }
+        res.send(succObj);
+    }
+}
